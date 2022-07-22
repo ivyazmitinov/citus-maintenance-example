f85b7f11d4db3393a6d95e0f659890d81d093de7
ivan.vyazmitinov <ivan.vyazmitinov@vizor-games.com>
2022-07-22 17:46:53 +0400

    Idempotent creation

--- src/main/resources/db/migration/V5__common_table.sql
+++ src/main/resources/db/migration/V5__common_table.sql
@@ -3,8 +3,14 @@ WITH databases AS (SELECT *
                                 ('another_citus_database')) AS t(db_name))
 SELECT DBLINK_EXEC(FORMAT('dbname=%I user=postgres', db_name), $remote$
 START TRANSACTION;
-CREATE TABLE test_table (user_id TEXT, data jsonb);
-SELECT create_distributed_table('test_table', 'user_id');
+CREATE TABLE IF NOT EXISTS test_table (user_id TEXT, data jsonb);
+DO $$
+BEGIN
+EXECUTE $cmd$SELECT create_distributed_table('test_table', 'user_id');$cmd$;
+EXCEPTION
+WHEN SQLSTATE '42P16' THEN
+    RETURN;
+END;$$;
 COMMIT;
 $remote$)
 FROM databases;
